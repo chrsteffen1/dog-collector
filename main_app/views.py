30158fc1ee0cccbc930ff0c0848c85c87a94f0aa
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-07 09:59:11 -0500

    add ORM

--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,18 @@
 from django.shortcuts import render
+from .models import Dog
 
-from django.http import HttpResponse
 
 
-class Dog:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
 # Create your views here.
 
 
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs})

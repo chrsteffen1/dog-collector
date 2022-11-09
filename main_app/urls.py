cfc68f7e4c0ec6ec1afd86f40c37eefd1a382cab
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-09 13:55:58 -0500

    add confirm delete view

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
   path('dogs/', views.dogs_index, name='dog_index'),
   path('dogs/<int:dog_id>/', views.dogs_detail, name='dogs_detail'),
   path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
-  path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name= 'dogs_update'),
-  path('dogs/<int:pk>/update/', views.DogDelete.as_view(), name= 'dogs_delete'),
+  path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
+  path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
 ]

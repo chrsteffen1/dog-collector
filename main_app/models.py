d532381d34b6f85e26dfb22b08fa66bec94a1321
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-10 15:28:36 -0500

    add toys

--- main_app/models.py
+++ main_app/models.py
@@ -18,6 +18,7 @@ class Dog(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
 	# new code below
   def __str__(self):

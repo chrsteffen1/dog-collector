8a946b3b70d8c08a73bfffed4b7425110dfb5936
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-14 00:07:51 -0500

    fix login route error

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
-  path('dogs/', views.dogs_index, name='dog_index'),
+  path('dogs/', views.dogs_index, name='dogs_index'),
   path('dogs/<int:dog_id>/', views.dogs_detail, name='dogs_detail'),
   path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
   path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
